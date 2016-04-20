507f4fa2e10a4a43db353e8d787489ca08ad853e
Pavel Yuhnovich <yuhnovichpaul@gmail.com>
2016-04-20 22:44:41 +0300

    retry added

--- task/09-functions-n-closures-tasks.js
+++ task/09-functions-n-closures-tasks.js
@@ -105,7 +105,16 @@ function memoize(func) {
  * retryer() => 2
  */
 function retry(func, attempts) {
-    throw new Error('Not implemented');
+     return () => {
+        while(attempts >= 0) {
+            try {
+                return func();        
+            }
+            catch(e) {
+                attempts--;
+            }    
+        }        
+    };   
 }
 
 
